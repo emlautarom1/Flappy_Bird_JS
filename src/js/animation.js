b99da934ed71fc3b7a125a31a1a118e50d1cb9a1
lautaroem1 <emlautarom1@gmail.com>
2019-08-11 02:13:57 -0300

    Animation class

---
+++ src/js/animation.js
@@ -0,0 +1,24 @@
+class PlayerAnimation {
+    constructor() {
+        this.playerSprites = [3, 31, 59, 31]
+        this.currentSprite = 0;
+        this.enabled = true;
+    }
+
+    update() {
+        if (!this.enabled) {
+            return;
+        } else {
+            this.enabled = false;
+            this.currentSprite++;
+            if (this.currentSprite === this.playerSprites.length) {
+                this.currentSprite = 0;
+            }
+            setTimeout(() => this.enabled = true, GLOBAL.PLAYER_WINGS_ANIMATION);
+        }
+    }
+
+    getPlayerSprite() {
+        return this.playerSprites[this.currentSprite];
+    }
+}
